bb9c65733235b14e4772c813bd67d60dc7119fd3
TilakRajRawat <tilakrajrawat1234@gmail.com>
2026-06-07 21:11:15 +0530

    Setup React application structure

---
+++ client/src/services/githubApi.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const API_BASE_URL = "http://localhost:5000/api/github";
+
+export const searchUser = async (username) => {
+  const response = await axios.get(
+    `${API_BASE_URL}/${username}`
+  );
+
+  return response.data;
+};
