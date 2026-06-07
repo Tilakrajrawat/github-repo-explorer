c2a822c5652f50f93375c99d74d3df133dc34f7c
TilakRajRawat <tilakrajrawat1234@gmail.com>
2026-06-07 20:40:25 +0530

    Transform GitHub API response structure

--- server/src/services/github.service.js
+++ server/src/services/github.service.js
@@ -6,9 +6,30 @@ const getGitHubUserData = async (username) => {
     axios.get(`https://api.github.com/users/${username}/repos`)
   ]);
 
+  const user = userResponse.data;
+
   return {
-    user: userResponse.data,
-    repos: reposResponse.data
+    user: {
+      avatarUrl: user.avatar_url,
+      name: user.name,
+      bio: user.bio,
+      followers: user.followers,
+      following: user.following,
+      publicRepos: user.public_repos,
+      profileUrl: user.html_url
+    },
+
+    repos: reposResponse.data.map(repo => ({
+      id: repo.id,
+      name: repo.name,
+      description: repo.description,
+      language: repo.language,
+      stars: repo.stargazers_count,
+      updatedAt: repo.updated_at,
+      openIssues: repo.open_issues_count,
+      defaultBranch: repo.default_branch,
+      repoUrl: repo.html_url
+    }))
   };
 };
 
