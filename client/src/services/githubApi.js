c618591f2f5371ff1cf2c410c7458ff772b3cfd8
TilakRajRawat <tilakrajrawat1234@gmail.com>
2026-06-08 14:35:06 +0530

    Configure frontend for production API

--- client/src/services/githubApi.js
+++ client/src/services/githubApi.js
@@ -1,7 +1,5 @@
 import axios from "axios";
-
-const API_BASE_URL = "http://localhost:5000/api/github";
-
+const API_BASE_URL = import.meta.env.VITE_API_URL;
 export const searchUser = async (username) => {
   const response = await axios.get(
     `${API_BASE_URL}/${username}`
